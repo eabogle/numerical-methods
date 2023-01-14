13ad33425a989632935b9985b33faa20c3fedb44
Emily Bogle <boglee@spu.edu>
2023-01-14 14:33:22 -0800

    writing of functions complete

--- Basic Single Step Methods.py
+++ Basic Single Step Methods.py
@@ -1,5 +1,3 @@
-import math
-
 t0 = 0
 tF = 4
 y0 = 5
@@ -7,18 +5,31 @@ h = 0.125
 
 t = t0
 y = y0
-print("%.8f" % t, "%.8f" % y)
+print(0, '\t'''"%.8f" % t, '\t'''"%.8f" % y)
+n = 1
+
+def fe(t, y, h):
+    def f(t, y):
+        return y * (1 - y)
+    return(y + h * f(t, y))
+
+def me(t, y, h):
+    def f(t, y):
+        return y * (1 - y)
+    return(y + h * f((t + h/2), (y + (h/2) * f(t, y))))
+
+def te(t, y, h):
+    def f(t, y):
+        return y * (1 - y)
+    return (y + (h/2) * (f(t, y) + f((t + h), (y + h * f(t, y)))))
+
 
 while (t+h <= tF):
+#while (n <= 8):
     y = fe(t, y, h)
     # y = me (t, y, h)
     # y = te(t, y, h)
     t = t + h
-    print("%.8f" % t, "%.8f" % y)
-
-
-
-
-
-
+    n = n + 1
+    print(n, '\t'''"%.8f" % t, '\t'"%.8f" % y)
 
