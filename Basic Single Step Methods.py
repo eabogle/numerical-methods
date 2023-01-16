2430e72f1f5b02836103d65f90729e8a3f88f840
Emily Bogle <boglee@spu.edu>
2023-01-16 15:24:31 -0800

    continuing hw 1 small changes

--- Basic Single Step Methods.py
+++ Basic Single Step Methods.py
@@ -1,36 +1,37 @@
+import math
 t0 = 0
-tF = 4
-y0 = 1
+tF = 2
+y0 = 5
 h = 0.25
 
 def yact(t):
-    return(pow(2/3 * pow(t,3) + 1, (1/2)))
+    return((5 * pow(math.e,t)) / (5 * pow(math.e,t) - 4))
 
 t = t0
 y = y0
 yactual = yact(t0)
 
-print('step', '\t''t', '\t''\t''y approx', '\t''\t''y actual')
+print('k', '\t''t', '\t''\t''\t''y_k', '\t''\t''\t''y(t_k)')
 print(0, '\t'''"%.8f" % t, '\t'''"%.8f" % y, '\t'"%.8f" % yactual)
 n = 1
 
 def fe(t, y, h):
     def f(t, y):
-        return ((1/y) * pow(t,2))
+        return (y*(1-y))
     return(y + h * f(t, y))
 
 def me(t, y, h):
     def f(t, y):
-        return ((1/y) * pow(t,2))
+        return (y*(1-y))
     return(y + h * f((t + h/2), y + (h/2) * f(t, y)))
 
 def te(t, y, h):
     def f(t, y):
-        return ((1/y) * pow(t,2))
+        return (y*(1-y))
     return (y + (h/2) * (f(t, y) + f((t + h), (y + h * f(t, y)))))
 
-#while (t+h <= tF):
-while (n <= 8):
+while (t+h <= tF):
+#while (n <= 8):
     # y = fe(t, y, h)
     # y = me (t, y, h)
     y = te(t, y, h)
