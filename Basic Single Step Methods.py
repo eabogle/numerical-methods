cbb5347489d1b8d68912b3192bb9b8c8deccca82
Emily Bogle <boglee@spu.edu>
2023-01-16 17:11:14 -0800

    error analysis

--- Basic Single Step Methods.py
+++ Basic Single Step Methods.py
@@ -10,18 +10,18 @@ h = (1/32)
 tF = (1-h)
 y0 = 0
 
-def yact(t):
-    return((5 * pow(math.e,t)) / (5 * pow(math.e,t) - 4))
+#def yact(t):
+    #return((5 * pow(math.e,t)) / (5 * pow(math.e,t) - 4))
 
 t = t0
 y = y0
-yactual = yact(t0)
+#yactual = yact(t0)
 tkarray = [t0]
 ykarray = [y0]
-yactarray = [y0]
+#yactarray = [y0]
 
 print('k', '\t''t', '\t''\t''\t''y_k', '\t''\t''\t''y(t_k)')
-print(0, '\t'''"%.8f" % t, '\t'''"%.8f" % y, '\t'"%.8f" % yactual)
+print(0, '\t'''"%.8f" % t, '\t'''"%.8f" % y)# '\t'"%.8f" % yactual)
 n = 1
 
 def fe(t, y, h):
@@ -44,15 +44,20 @@ while (t+h <= tF):
     y = fe(t, y, h)
     # y = me (t, y, h)
     # y = te(t, y, h)
-    yactual = yact(t + h)
-    print(n, '\t'''"%.8f" % t, '\t'"%.8f" % y, '\t'"%.8f" % yactual)
+    #yactual = yact(t + h)
+    print(n, '\t'''"%.8f" % t, '\t'"%.8f" % y)# '\t'"%.8f" % yactual)
     t = t + h
     n = n + 1
     tkarray.append(t)
     ykarray.append(y)
-    yactarray.append(yactual)
+    #yactarray.append(yactual)
+
+yact31 = 1.5230194019969561110
+specerror = abs(yact31 - y)
+print('|y(1-h) - y_31| = ', specerror)
 
 plt.plot(tkarray, ykarray)
 plt.xlabel('t_k')
 plt.ylabel('y_k')
+plt.title('Inverse Error Function Approx. y, Forward Euler')
 plt.show()
