6e01a41ff42b2e70d08adb2aecef102ae5d71144
Emily Bogle <boglee@spu.edu>
2023-01-17 18:35:23 -0800

    final save for the night

--- Errors Basic Single Step Methods.py
+++ Errors Basic Single Step Methods.py
@@ -32,15 +32,17 @@ lamb = (-1) * 30
 emaxold = 0.0
 h = 0.25
 
-print('h', '\t''max error','\t''\t''\t''error ratio')
+print('h', '\t''\t''\t''max error', '\t''\t''\t''error ratio')
 
 while (h >= hmin):
+    print('test1')
     emax = 0
     tmax = 0
     t = t0
     y = y0
 
     while (t + h <= tF):
+        print('test2')
         y = fe(t, y, h)
         #y = bee(t, y, h)
         #y = m(t, y, h)
