b79d383f618204740a0716be5217f1f09dc6e88d
Emily Bogle <boglee@spu.edu>
2023-01-19 13:10:14 -0800

    finished code try 1

--- Errors Basic Single Step Methods.py
+++ Errors Basic Single Step Methods.py
@@ -9,7 +9,25 @@ lamb = (-1) * 30
 
 #Defining functions
 def f(t,y):
-    return(lam * (y - math.cos(t)) - math.sin(t))
+    return(lamb * (y - math.cos(t)) - math.sin(t))
+
+def yact(t):
+    return(math.cos(t))
+
+def fe(t, y, h):
+    return(y + h * f(t, y))
+
+def bee(t, y, h):
+    return((1 / (1 - h * lamb)) * y - (h / (1 - h * lamb)) * (lamb * math.cos(t + h) + math.sin(t + h)))
+
+def m(t, y, h):
+    return(y + h * f((t + h/2), y + (h/2) * f(t, y)))
+
+def te(t, y, h):
+    return((1 + (h * lamb)/2) * y - (h/2) * (lamb * math.cos(t) - math.sin(t)))
+
+def tpc(t, y, h):
+    return(y + (h/2) * (f(t, y) + f((t + h), (y + h * f(t, y)))))
 
 #Initialization
 t = tzero
@@ -17,26 +35,34 @@ y = yzero
 emaxold = 0
 h = 0.25
 
-print('h', '\t''\t''\t''max error', '\t''\t''\t''error ratio')
+print('Forward Euler Explicit')
+print('h', '\t''\t''max error', '\t''\t''error ratio')
 
 #First while loop for h values
 while (h >= hmin):
-    print(h)
-    emax = 0
+    emaximum = 0
     tmax = 0
     t = tzero
     y = yzero
 
-    while ((t + h) <= tF):
-        print(t)
+    #Second while loop for time steps
+    while (t <= tF):
         # y = fe(t, y, h)
         # y = bee(t, y, h)
         # y = m(t, y, h)
-        # y = te(t, y, h)
+        y = te(t, y, h)
         # y = tpc(t, y, h)
         t = t + h
-        # actual = yact(t)
-        # e = abs(y - actual)
+        actual = yact(t)
+        err = abs(y - actual)
+
+        if (err > emaximum):
+            emaximum = err
+
+    ratio = emaxold / emaximum
+    print(h, '\t''\t'''"%.8f" % emaximum, '\t''\t'''"%.8f" % ratio)
+    h = h * (0.5)
+    emaxold = emaximum
 
 
 
