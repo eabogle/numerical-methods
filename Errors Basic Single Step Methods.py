e6d86f22249222202c7c5428161428ee1f3552ba
Emily Bogle <boglee@spu.edu>
2023-01-19 18:40:18 -0800

    finished

--- Errors Basic Single Step Methods.py
+++ Errors Basic Single Step Methods.py
@@ -2,28 +2,47 @@ import math
 
 #Defining variables
 tzero = 0
-tF = math.pi
-#tF = 4/3
+#tF = math.pi
+tF = 1
 hmin = 1 / pow(2, 11)
 yzero = 1
-lamb = (-1) * 30
-#lamb =
+#lamb = (-1) * 30
 
 #Defining functions
 def f(t,y):
-    return(lamb * (y - math.cos(t)) - math.sin(t))
-    #return((-1) * pow(abs(y), (1/4)))
+    #return(lamb * (y - math.cos(t)) - math.sin(t))
+    return((-1) * pow(abs(y), (1/4)))
+
+def df(t,y):
+    #return(lamb)
+    if (y > 0):
+        return((-1/4) * pow(y, -3/4))
+    if (y < 0):
+        return((1/4) * pow(-y, -3/4))
 
 def yact(t):
-    return(math.cos(t))
-    #return(pow(1 - (3/4) * t), 4/3)
+    #return(math.cos(t))
+    return(pow((1 - (3/4) * t), 4/3))
 
 def fe(t, y, h):
     return(y + h * f(t, y))
 
 def bee(t, y, h):
-    #return((1 / (1 - h * lamb)) * y - (h / (1 - h * lamb)) * (lamb * math.cos(t + h) + math.sin(t + h)))
-    return()
+    return((1 / (1 - h * lamb)) * y - (h / (1 - h * lamb)) * (lamb * math.cos(t + h) + math.sin(t + h)))
+
+def be(t, y, h):
+    j = 0
+    jmax = 2
+
+    x = y
+
+    while (j < jmax):
+        F = y + h * f(t + h, x) - x
+        dF = h * df(t + h, x) - 1
+        x = x - (F / dF)
+        j = j + 1
+
+    return (x)
 
 def m(t, y, h):
     return(y + h * f((t + h/2), y + (h/2) * f(t, y)))
@@ -34,7 +53,19 @@ def te(t, y, h):
            ((-1)* lamb * math.cos(t) - math.sin(t) - (lamb * math.cos(t + h)) - math.sin(t+h)))
 
 
-    #return()
+def trap(t, y, h):
+    j = 0
+    jmax = 2
+
+    x = y
+
+    while (j < jmax):
+        F = y + (h/2) * (f(t, y) + f(t+h,x)) - x
+        dF = (h/2) * df(t + h, x) - 1
+        x = x - (F / dF)
+        j = j + 1
+
+    return (x)
 
 def tpc(t, y, h):
     return(y + (h/2) * (f(t, y) + f((t + h), (y + h * f(t, y)))))
@@ -45,7 +76,7 @@ y = yzero
 emaxold = 0
 h = 0.25
 
-print('Trapezoid Explicit')
+print('Trapezoid Predictor Corrector')
 print('h', '\t''\t''max error', '\t''\t''error ratio')
 
 #First while loop for h values
@@ -59,9 +90,11 @@ while (h >= hmin):
     while (t <= tF):
         # y = fe(t, y, h)
         # y = bee(t, y, h)
+        # y = be(t, y, h)
         # y = m(t, y, h)
-        y = te(t, y, h)
-        # y = tpc(t, y, h)
+        # y = te(t, y, h)
+        # y = trap(t, y, h)
+        y = tpc(t, y, h)
         t = t + h
         actual = yact(t)
         err = abs(y - actual)
